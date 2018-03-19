0d77162341f090f6b61a02f4ec4bbebc9f8d7cdc
0151n <oisinohall@gmail.com>
2018-03-19 23:49:21 +0000

    added method to block class to randomly generate boards, and test code to main method

--- src/aMAZEing.java
+++ src/aMAZEing.java
@@ -6,5 +6,8 @@ public class aMAZEing {
 
 	public static void main(String[] args) {
 		System.out.println("Hello There");
+		Board board = new Board(4,4);
+		board.CreateRandomBoard();
+		System.out.print(board);
 	}
 }
